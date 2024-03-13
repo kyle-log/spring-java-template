1ff7debe0e69860c821946ce123dda7994d8b5d8
Kyle <cocomo1316@gmail.com>
2024-03-13 23:00:01 +0900

    API for login

--- src/main/java/com/kylelog/springjavatemplate/controller/UserController.java
+++ src/main/java/com/kylelog/springjavatemplate/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.kylelog.springjavatemplate.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     @PostMapping("/signup")
@@ -12,7 +17,8 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String login(String email, String password) {
+    public String login(String email, String password, HttpServletRequest request) {
+        request.setAttribute("email", email);
         return "Hello, " + email + "!";
     }
 }
