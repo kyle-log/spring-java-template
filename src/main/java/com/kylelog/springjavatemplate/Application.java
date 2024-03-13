f417ccff64c440f17a4e840652ae229d8629f7b0
Kyle <cocomo1316@gmail.com>
2024-03-13 21:12:52 +0900

    Second commit

--- src/main/java/com/kylelog/springjavatemplate/Application.java
+++ src/main/java/com/kylelog/springjavatemplate/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJavaTemplateApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJavaTemplateApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
