32092565589681e85adec5d96650cfd03fa2a098
Kyle <cocomo1316@gmail.com>
2024-03-13 21:01:09 +0900

    First commit

---
+++ api/src/main/java/com/kylelog/springjavatemplate/Application.java
@@ -0,0 +1,13 @@
+package com.kylelog.springjavatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaTemplateApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/com/kylelog/springjavatemplate/SpringJavaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylelog.springjavatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
